d9b47a6d16bb7cfa582c834492ab19757b351ec8
神的孩子都在歌唱 <1156028692@qq.com>
2021-11-02 13:29:15 +0800

    开始博客后台的搭建

---
+++ blog-admin/src/test/java/com/chen/blogadmin/BlogAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chen.blogadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
